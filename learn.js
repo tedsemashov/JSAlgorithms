0b2bcf84599ee56ad221b2e10c024162ef9d49f5
tedsemashov <ted.semashov@gmail.com>
2017-12-24 02:31:44 +0200

    filter method

--- learn.js
+++ learn.js
@@ -1058,3 +1058,12 @@ function addTogether() {
 // test here
 addTogether(2,3);
 
+
+// filter
+function findElement(arr, func) {
+   var num = 0;
+   num = arr.filter(func);
+   return num[0];
+}
+
+findElement([1, 2, 3, 4], function(num){ return num % 2 === 0; });
